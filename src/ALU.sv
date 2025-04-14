996ee910a7a8bca16392f38e73f1be9519e9bb09
Amit Kumar <amitkh@gmail.com>
2025-04-14 16:00:09 -0400

    some more small fixes

--- src/ALU.sv
+++ src/ALU.sv
@@ -1,8 +1,6 @@
 // ALU and its input mutliplexers
 
-module ALU #(
-
-)(
+module ALU (
     input logic [31:0] PC,
     input logic [31:0] PC_old,
     input logic [4:0] rs1v,
@@ -15,122 +13,121 @@ module ALU #(
     output logic zero
 );
 
-parameter increment = 4;
-logic [31:0] src1_result = 0;
-logic [31:0] src2_result = 0;
+  parameter increment = 4;
+  logic [31:0] src1_result = 0;
+  logic [31:0] src2_result = 0;
 
-always_comb begin
+  always_comb begin
     case (ALU_src1_sel)
-        2'd0: begin
-            src1_result <= PC;
-        end
-        2'd1: begin
-            src1_result <= PC_old;
-        end
-        2'd2: begin
-            src1_result <= rs1v;
-        end
-        3'd3: begin
-            src1_result <= src1_result;
-        end
+      2'd0: begin
+        src1_result = PC;
+      end
+      2'd1: begin
+        src1_result = PC_old;
+      end
+      2'd2: begin
+        src1_result = rs1v;
+      end
+      3'd3: begin
+        src1_result = src1_result;
+      end
     endcase
-end
+  end
 
-always_comb begin
+  always_comb begin
     case (ALU_src2_sel)
-        2'd0: begin
-           src2_result <= rs2v; 
-        end
-        2'd1: begin
-            src2_result <= imm_ext;
-        end 
-        2'd2: begin
-            src2_result <= increment;
-        end
-        2'd3: begin
-            src2_result <= src2_result;
-        end
+      2'd0: begin
+        src2_result = rs2v;
+      end
+      2'd1: begin
+        src2_result = imm_ext;
+      end
+      2'd2: begin
+        src2_result = increment;
+      end
+      2'd3: begin
+        src2_result = src2_result;
+      end
     endcase
-end
+  end
 
-always_comb begin
+  always_comb begin
     case (ALU_ctrl)
-        2'd0: begin
-            //Return the sum of src1 and 2
-            ALU_result <= src1_result + src2_result;
+      4'd0: begin
+        //Return the sum of src1 and 2
+        ALU_result = src1_result + src2_result;
+      end
+      4'd1: begin
+        //Return the difference between src1 & 2, set zero wire high if they are equal
+        ALU_result = src1_result - src2_result;
+        if (ALU_result == 0) begin
+          zero = 1;
         end
-        2'd1: begin
-            //Return the difference between src1 & 2, set zero wire high if they are equal
-            ALU_result <= src1_result - src2_result;
-            if(ALU_result == 0) begin
-                zero <= 1;
-            end
+      end
+      4'd2: begin
+        //Return a list of bits that is a bit wise and of src1 and 2
+        ALU_result = src1_result && src2_result;
+      end
+      4'd3: begin
+        //Return a list of bits that is a bit wise or of src1 and 2
+        ALU_result = src1_result || src2_result;
+      end
+      4'd4: begin
+        //Return a list of bits that is a bit wise xor of src1 and 2
+        ALU_result = (src1_result || src2_result) && ~(src1_result && src2_result);
+      end
+      4'd5: begin
+        //Return the lower value of src1 and 2 assuming they are signed
+        if ($signed(src1_result) > $signed(src2_result)) begin
+          ALU_result = src2_result;
+        end else begin
+          ALU_result = src1_result;
         end
-        2'd2: begin
-            //Return a list of bits that is a bit wise and of src1 and 2
-            ALU_result <= src1_result && src2_result; 
+      end
+      4'd6: begin
+        //Return the lower value of src1 and 2 assuming they are unsigned
+        if (src1_result > src2_result) begin
+          ALU_result = src2_result;
+        end else begin
+          ALU_result = src1_result;
         end
-        2'd3: begin
-            //Return a list of bits that is a bit wise or of src1 and 2
-            ALU_result <= src1_result || src2_result; 
-        end
-        2'd4: begin
-            //Return a list of bits that is a bit wise xor of src1 and 2
-            ALU_result <= (src1_result || src2_result) && ~(src1_result && src2_result);
-        end
-        2'd5: begin
-            //Return the lower value of src1 and 2 assuming they are signed
-            if($signed(src1_result)>$signed(src2_result)) begin
-                ALU_result <= src2_result;
-            end
-            else begin
-                ALU_result <= src1_result;
-            end
-        end
-        2'd6: begin
-            //Return the lower value of src1 and 2 assuming they are unsigned
-            if(src1_result>src2_result) begin
-                ALU_result <= src2_result;
-            end
-            else begin
-                ALU_result <= src1_result;
-            end
-        end
-        2'd7: begin
-            //Shift the src1 value left by a number of bits equal to the lower 5 bits of src2
-            shift_value = src2_result[4:0];
-            ALU_result = {src1_result[31-shift_value:0], shift_value'b0};
-        end
-        2'd8: begin
-            //Shift the src1 value right by a number of bits equal to the lower 5 bits of src2
-            shift_value = src2_result[4:0];
-            ALU_result = {shift_value'b0, src1_result[31:shift_value]};
-        end
-        2'd9: begin
-            //Shift the src1 value right by a number of bits equal to the lower 5 bits of src2 and preserve the sign bit
-            shift_value = src2_result[4:0];
-            ALU_result = {src1_result[31], shift_value'b0, src1_result[30:shift_value]};
-        end
-        2'd10: begin
+      end
+      4'd7: begin
+        //Shift the src1 value left by a number of bits equal to the lower 5 bits of src2
+        shift_value = src2_result[4:0];
+        ALU_result  = {src1_result[31-shift_value:0], '0};
+      end
+      4'd8: begin
+        //Shift the src1 value right by a number of bits equal to the lower 5 bits of src2
+        shift_value = src2_result[4:0];
+        ALU_result  = {'0, src1_result[31:shift_value]};
+      end
+      4'd9: begin
+        //Shift the src1 value right by a number of bits equal to the lower 5 bits of src2 and preserve the sign bit
+        shift_value = src2_result[4:0];
+        ALU_result  = {src1_result[31], '0, src1_result[30:shift_value]};
+      end
+      4'd10: begin
 
-        end
-        2'd11: begin
-            
-        end
-        2'd12: begin
+      end
+      4'd11: begin
 
-        end
-        2'd13: begin
+      end
+      4'd12: begin
 
-        end
-        2'd14: begin
+      end
+      4'd13: begin
 
-        end
-        2'd15: begin
+      end
+      4'd14: begin
 
-        end
+      end
+      4'd15: begin
+
+      end
     endcase
 
-end
+  end
 
 endmodule
+
--- src/Result.sv
+++ src/Result.sv
@@ -1,37 +1,35 @@
 // mux and flip flop of output of ALU
 
-module Result #(
-
-)(
+module Result (
     input logic clk,
-    input logic ALU_result,
+    input logic [31:0] ALU_result,
     input logic [31:0] data,
     input logic [1:0] result_sel,
     output logic [31:0] result
 );
 
+  logic [31:0] ALU_out;
 
-logic ALU_out [31:0];
-
-always_comb begin
+  always_comb begin
     case (result_sel)
-        2'd0: begin
-            result <= ALU_out;
-        end
-        2'd1: begin
-            result <= data;
-        end
-        2'd2: begin
-            result <= ALU_result;
-        end
-        2'd3: begin
-            result <= result;
-        end
+      2'd0: begin
+        result = ALU_out;
+      end
+      2'd1: begin
+        result = data;
+      end
+      2'd2: begin
+        result = ALU_result;
+      end
+      2'd3: begin
+        result = result;
+      end
     endcase
-end
+  end
 
-always_ff @(posedge clk) begin
-    ALU_out = ALU_result;
-end
+  always_ff @(posedge clk) begin
+    ALU_out <= ALU_result;
+  end
 
 endmodule
+
